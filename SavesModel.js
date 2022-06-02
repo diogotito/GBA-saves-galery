75d1c22e930657f55b59b9ac3ec36b44a25cb170
diogotito <diogotitomarques@gmail.com>
2022-06-02 02:49:04 +0100

    Cache revisions & rating w/ SVG spritesheet

--- SavesModel.js
+++ SavesModel.js
@@ -15,9 +15,50 @@ function* listSaves() {
 }
 
 
-const cleanupROMName = name =>
-  /^(?:\d{4} - )?(?<name>.*?)(?: \(\w\) ?\(\w+\))?$/
-    .exec(name)?.groups["name"] ?? name
+/** Uses the script cache and the advanced Drive API */
+function getRevisions(file) {
+  const ONE_DAY = 24 * 60 * 60;
+  const cache = CacheService.getScriptCache();
+  
+  // Try to retrieve from cache and return the cached value if successful
+  const key = file.getName() + "/" + file.getLastUpdated().toISOString()
+  let value = cache.get(key)
+  if (value !== null) {
+    console.log(`Retrieved value for ${key}:\n${value}`)
+    return JSON.parse(value)
+  }
+
+  // Fetch file revisions (slow!) and simplify the results
+  let revisions = Drive.Revisions.list(file.getId()).items
+  revisions = revisions.map( ({modifiedDate, downloadUrl}) => ({modifiedDate, downloadUrl}) )
+  
+  // Cache the result
+  value = JSON.stringify(revisions)
+  cache.put(key, value, ONE_DAY)
+  console.log(`-> STORED value for ${key}:\n${value}`)
+
+  return revisions
+}
+
+
+const cleanupROMName = romName =>
+  /^(?:\d{4} - )?(?<NAME>.*?)(?: \(\w\) ?\(.*\))?$/
+    .exec(romName)?.groups["NAME"] ?? romName
+
+
+function parseDescription(descText) {
+  const descLines = descText.split('\n')
+  
+  const review = descLines.filter(line => !line.startsWith("* ")).join('\n')
+  
+  const ratingText = descLines.filter(line => line.startsWith("* ")).join('\n')
+  const ratings = {}
+  for (const [, name, score, _maxScore] of ratingText.matchAll(/\*\s*([^:]+): ([\d.]+)\/(\d+)/g)) {
+    ratings[name] = parseFloat(score)
+  }
+
+  return { review, ratings }
+}
 
 
 function getAllGames() {
@@ -37,20 +78,20 @@ function getAllGames() {
             image: null,
             numRevisions: 0,
           })),
-          description: "",
+          description: { review: "", ratings: {} },
           cartridgeSave: null,
           numRevisions: 0,
         }
       }
       if (ext == ".sav") {
-        games[game].description = file.getDescription()
+        games[game].description = parseDescription(file.getDescription())
         games[game].cartridgeSave = file.getLastUpdated()
-        games[game].numRevisions = Drive.Revisions.list(file.getId()).items.length
+        games[game].numRevisions = getRevisions(file).length
       } else if (ext.endsWith(".png")) {
         games[game].saveStates[slot].image = file.getDownloadUrl()
+        games[game].saveStates[slot].numRevisions = getRevisions(file).length
       } else if (ext.startsWith(".st")) {
         games[game].saveStates[slot].lastSaved = file.getLastUpdated()
-        games[game].saveStates[slot].numRevisions = Drive.Revisions.list(file.getId()).items.length
       }
     }
   }
